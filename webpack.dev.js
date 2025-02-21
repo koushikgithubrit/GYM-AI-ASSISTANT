8d88cf0b96b6b6d4c628240cd7fcefa5ac157aeb
koushikgithubrit <koushikadakka2004@gmail.com>
2025-02-21 11:58:17 +0530

    dev pack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,15 @@
+// /* eslint-disable import/no-extraneous-dependencies */
+// const path = require("path");
+// const { merge } = require("webpack-merge");
+// const common = require("./webpack.common");
+
+// module.exports = merge(common, {
+//   mode: "development",
+//   output: {
+//     path: path.resolve(__dirname, "dist"),
+//     filename: "bundle-dev.js",
+//   },
+// });
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require("path");
 const { merge } = require("webpack-merge");
@@ -9,4 +21,14 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle-dev.js",
   },
+  devServer: {
+    host: "192.168.169.157", // Allow access from any network
+    port: 8080, // Change if needed
+    allowedHosts: "all", // Fixes "Invalid Host header" error
+    historyApiFallback: true, // Fixes issues with React Router (if used)
+    hot: true, // Enables Hot Module Replacement
+    client: {
+      webSocketURL: "auto://0.0.0.0:0/ws" // Fix WebSocket live reload issues
+    }
+  }
 });
