f18051412b6bca0b018fedae7c9acdf4a136960d
koushikgithubrit <koushikadakka2004@gmail.com>
2025-02-23 00:06:11 +0530

    remove login page

--- gymlytics/backend/models/Leaderboard.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose'); // Add this line
-
-const LeaderboardSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    score: { type: Number, required: true },
-});
-
-const Leaderboard = mongoose.model('Leaderboard', LeaderboardSchema);
-module.exports = Leaderboard;
--- gymlytics/backend/models/Score.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ScoreSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    score: { type: Number, required: true }
-});
-
-const Score = mongoose.model("Score", ScoreSchema);
-module.exports = Score;
